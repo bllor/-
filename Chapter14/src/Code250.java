8d067f5ad61d1a74ac18780a025cbd16c56bde6b
bllor <duki0105@gmail.com>
2023-06-18 23:29:23 +0900

    Code250

--- Chapter14/src/Code250.java
+++ Chapter14/src/Code250.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 /*
@@ -20,9 +22,27 @@ class pe3{
 }
 public class Code250 {
 	public static void main(String[] args) {
-		TreeMap<Integer, String> map= new TreeMap<>();
-		//map.put(3,new pe3("a",10) );
-	
+		TreeMap<Integer, pe3> map= new TreeMap<>();
+		map.put(3,new pe3("a",10));
+		map.put(2,new pe3("f",20));
+		map.put(5,new pe3("e",40));
+		map.put(4,new pe3("d",30));
+		map.put(1,new pe3("c",60));
+		map.put(6,new pe3("b",50));
+		
+		Set<Integer>keySet= map.keySet();
+		System.out.println(keySet);
+		System.out.println("-----------------");
+		
+		for(Map.Entry<Integer, pe3> e: map.entrySet()) {
+			Integer key= e.getKey();
+			pe3 value= e.getValue();
+			System.out.println(key+":"+value);
+		}
+			System.out.println("--------------");
+			pe3 val= (pe3)map.get(3);
+			System.out.print("key 3 ->");
+			System.out.println(val);
 	
 	
 	
